eaf18ee134167252795ad50d79a448b3d5c03d54
YongLuo <834704671@qq.com>
2022-01-18 17:51:37 +0800

    修改协议规范以及示例

---
+++ src/main/java/application/module/GameProtocols.java
@@ -0,0 +1,16 @@
+package application.module;
+
+/**
+ * 模块协议起始
+ *
+ * @author Luo Yong
+ * @date 2022-1-17
+ * @Source 1.0
+ */
+public interface GameProtocols {
+
+    int EXAMPLE = 1000;
+
+    int USER = 1010;
+
+}
