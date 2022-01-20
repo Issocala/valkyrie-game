3b0aca3d829649416300d0ef8b4919696cf905d7
YongLuo <834704671@qq.com>
2022-01-20 16:08:08 +0800

    新增 idUtils 修改注释

---
+++ src/main/java/application/guid/IdUtils.java
@@ -0,0 +1,50 @@
+package application.guid;
+
+/**
+ * ID生成器工具类
+ *
+ * @author Luo Yong
+ * @date 2022-1-10
+ * @Source 1.0
+ */
+public class IdUtils {
+    /**
+     * 获取随机UUID
+     *
+     * @return 随机UUID
+     */
+    public static String randomUUID() {
+        return UUID.randomUUID().toString();
+    }
+
+    /**
+     * 简化的UUID，去掉了横线
+     *
+     * @return 简化的UUID，去掉了横线
+     */
+    public static String simpleUUID() {
+        return UUID.randomUUID().toString(true);
+    }
+
+    /**
+     * 获取随机UUID，使用性能更好的ThreadLocalRandom生成UUID
+     *
+     * @return 随机UUID
+     */
+    public static String fastUUID() {
+        return UUID.fastUUID().toString();
+    }
+
+    /**
+     * 简化的UUID，去掉了横线，使用性能更好的ThreadLocalRandom生成UUID
+     *
+     * @return 简化的UUID，去掉了横线
+     */
+    public static String fastSimpleUUID() {
+        return UUID.fastUUID().toString(true);
+    }
+
+    public static long fastSimpleUUIDLong() {
+        return UUID.fastUUID().getMostSignificantBits() & Long.MAX_VALUE;
+    }
+}
--- src/main/java/application/util/TimeMacro.java
+++ src/main/java/application/util/TimeMacro.java
@@ -9,29 +9,14 @@ package application.util;
  */
 public final class TimeMacro {
 
-    /**
-     * 一秒
-     */
     public static final long SECOND = 1000L;
 
-    /**
-     * 一分钟
-     */
     public static final long MINUTE = 60000L;
 
-    /**
-     * 一小时
-     */
     public static final long HOUR = 3600000L;
 
-    /**
-     * 一天
-     */
     public static final long DAY = 86400000L;
 
-    /**
-     * 一周
-     */
     public static final long WEEK = 7 * DAY;
 
 }
