06397d1aaaeede56a086290134dc6da3a33e81af
YongLuo <834704671@qq.com>
2022-03-18 17:26:10 +0800

    add OrganismType

---
+++ src/main/java/application/module/organism/OrganismType.java
@@ -0,0 +1,13 @@
+package application.module.organism;
+
+/**
+ * @author Luo Yong
+ * @date 2022-3-16
+ * @Source 1.0
+ */
+public interface OrganismType {
+
+    byte PLAYER = 1;
+
+    byte BOSS = 2;
+}
