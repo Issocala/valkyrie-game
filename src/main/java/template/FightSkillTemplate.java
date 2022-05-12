2da8287141bc616821ed0c3c1fd1d996a75ce1e6
YongLuo <834704671@qq.com>
2022-05-12 10:30:42 +0800

    template

--- src/main/java/template/FightSkillTemplate.java
+++ src/main/java/template/FightSkillTemplate.java
@@ -1,7 +1,7 @@
 /* 由程序自动生成，请勿修改。*/
 package template;
 
-public record FightSkillTemplate(int id,byte skillType,byte activeSkillType,byte skillTargetType,byte skillAimType,long cdTime,int costHp,int costMp,int[] skillProcess,int triggerSkill){
+public record FightSkillTemplate(int id,byte skillType,byte activeSkillType,byte skillTargetType,byte skillAimType,long cdTime,int costHp,int costMp,int[] skillProcess,int skillGroup){
 
     public static FightSkillTemplate parse(CustomByteBuffer cbb){
         var id = cbb.getInt();
@@ -17,9 +17,9 @@ public record FightSkillTemplate(int id,byte skillType,byte activeSkillType,byte
         for (int i = 0; i < skillProcessLength; i++){
             skillProcess[i] = cbb.getInt();
         }
-        var triggerSkill = cbb.getInt();
+        var skillGroup = cbb.getInt();
 
-        var temp = new FightSkillTemplate(id,skillType,activeSkillType,skillTargetType,skillAimType,cdTime,costHp,costMp,skillProcess,triggerSkill);
+        var temp = new FightSkillTemplate(id,skillType,activeSkillType,skillTargetType,skillAimType,cdTime,costHp,costMp,skillProcess,skillGroup);
         return temp;
     }
 }
